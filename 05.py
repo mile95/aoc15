ea1b49b97a905c60f063bee0487998eec031dc35
Fredrik Mile <fredrik.mile@gmail.com>
2021-07-24 23:55:34 +0200

    Add missing solution for 05 part A

--- 05.py
+++ 05.py
@@ -3,6 +3,16 @@ import re
 with open("input.txt") as f:
     data = f.read().split('\n')
 
+# Part A
+count = 0
+for s in data:
+    vowels = s.count('a') + s.count('e') + s.count('i') + s.count('o') + s.count('u')
+    letter_twice = [s[x] == s[x+1] for x in range(len(s) - 1)]
+    forbidden_strings = [x in s for x in ['ab','cd', 'pq', 'xy']]
+    if (vowels >= 3) and (True in letter_twice) and (True not in forbidden_strings):
+        count += 1
+print(count)
+
 # Part B
 count = 0
 for word in data:
